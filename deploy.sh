4a85b55b7ab935854df2f5f40c63566223ff9841
Maybe-Hao <1348114176@qq.com>
2022-02-21 22:21:04 +0800

    部署

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:xiaojubusongxi26/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Maybe-Hao/daily.git master:gh-pages
+git push -f git@github.com:Maybe-Hao/daily.git main:gh-pages
 
 cd -
